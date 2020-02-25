1702b8b6a2f333671296b5004766dad5f025628f
Bckant20 <60886219+Bckant20@users.noreply.github.com>
2020-02-25 08:53:51 -0500

    Add files via upload

---
+++ Group B.py
@@ -0,0 +1,30 @@
+import random
+randomWords = ["ducks", "jumbo", "lucky", "pills", "flour"]
+secret = random.choice (randomWords)
+letter = ""
+updateWord = []
+def initialize():
+    print "We have a secret word"
+    print "_ _ _ _ _"
+def getLetter():
+    print ("Enter a letter")
+    global letter
+    letter = raw_input()
+def ifWon():
+    if secret == updateWord:
+        print ("you win")
+    else:
+        getLetter()
+def main():
+    initialize ()
+    getLetter ()
+    ifWon()
+main()
+    
+#def tester():
+    #print updateWord
+    #updateWord = raw_input()
+    #if updateWord== secret:
+        #print ("okay")
+    #else: 
+        #print ("no")
